a6a4e485e27c76f8137b67a42ae5f513f02e0dbf
hecto <hecto@Hector>
2025-03-25 08:42:49 +0100

    Funciones de resta y multiplicación añadidas

--- nuevoProyectoED/src/nuevoProyectoED/Calculadora.java
+++ nuevoProyectoED/src/nuevoProyectoED/Calculadora.java
@@ -4,7 +4,15 @@ public class Calculadora {
 	public int sumar(int a, int b) {
 		return a + b;
 	}
+
+	public int restar(int a, int b) {
+		return a - b;
+	}
 	
+	public int multiplicar(int a, int b) {
+		return a * b;
+	}
+
 	public static void main(String[] args) {
 		Calculadora calc = new Calculadora();
 		System.out.println("Resultado de la suma: " +
